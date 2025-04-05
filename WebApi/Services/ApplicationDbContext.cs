e9223ba3243e8620eed99fb0ade2d9ceb009ada4
andremacedo <andredekamacedo@gmail.com>
2025-04-05 00:12:49 -0300

    comit initil

---
+++ WebApi/Services/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Services;
+
+public class ApplicationDbContext: IdentityDbContext
+{
+     public ApplicationDbContext(DbContextOptions options): base(options)
+     {
+          
+     }
+    
+}
